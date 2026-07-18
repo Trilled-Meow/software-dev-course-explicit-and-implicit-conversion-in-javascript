1f989af833341c025f65c96cb754fe9ee66c555a
jrigsby <jennifernrigsby@gmail.com>
2026-07-18 15:19:39 -0500

    Corrects the code and adds two additional examples of implicit and explicit data type converstion in JavaScript

--- explicit-and-implicit-conversion-in-javascript.js
+++ explicit-and-implicit-conversion-in-javascript.js
@@ -19,14 +19,27 @@ Use console.log() to clearly show the before-and-after type conversions.
 */
 
 
-let result = "5" - 2;
+let result = "5" + String( - 2); //Changed -2 to a string to ensure proper concatenation instead of subtraction
 console.log("The result is: " + result);
 
-let isValid = Boolean("false");
+let isValid = Boolean(""); //Changed the value to the Boolean function to be an empty string, making it a falsy value
 if (isValid) {
     console.log("This is valid!");
-}
+} else {
+      console.log("This is not valid!"); //Added an else statement to clarify code correction via console.log() output
+    }
 
 let age = "25";
-let totalAge = age + 5;
+let totalAge = Number(age) + 5; //Converted age to a number to avoid the numbers concatonating as strings
 console.log("Total Age: " + totalAge);
+
+let catName = "Oliva";
+let catAge = 2;
+console.log(`${catName} is ${catAge} years old.`); //example of implicit type conversion using template literals, which automatically convert variables to strings to avoid tedious manual concatenation
+
+let undefinedVar;
+console.log(typeof undefinedVar); //demonstrates data type as undefined
+booksReadAnnualTotal = 46;
+booksReadJuly = 5;
+undefinedVar = booksReadAnnualTotal + booksReadJuly; //Implicit conversion of undefined to number
+console.log("Books Read: " + String(booksReadAnnualTotal + booksReadJuly)); //Explicitly converts the sum from numbers to a string
